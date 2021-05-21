11f6750f5b8adcb39238feb773d4554ee56e8e58
Белоногова Алена <belonogova@ians.aero>
2021-05-21 14:57:41 +0300

    Тесты

--- rqadviser/clustering/tests/common.py
+++ rqadviser/clustering/tests/common.py
@@ -4,4 +4,4 @@
 from pathlib import Path
 
 RESOURCES_PATH = Path.joinpath(Path(__file__).parent, "resources")
-TEST_CSV_PATH = Path.joinpath(RESOURCES_PATH, "test.csv")
+TEST_CSV_PATH = Path.joinpath(RESOURCES_PATH, "tests.csv")
---
+++ rqadviser/controller/tests/common.py
@@ -0,0 +1,6 @@
+"""
+Общие данные для тестов контроллера
+"""
+from pathlib import Path
+
+OUTPUT_PATH = Path.joinpath(Path(__file__).parent, "output")
