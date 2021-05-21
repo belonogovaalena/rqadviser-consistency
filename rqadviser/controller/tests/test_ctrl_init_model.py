bd0d27389e3073a46a2259ee0dab085c75103bde
Белоногова Алена <belonogova@ians.aero>
2021-05-21 14:58:05 +0300

    Тесты

---
+++ rqadviser/controller/tests/test_ctrl_init_model.py
@@ -0,0 +1,39 @@
+# """
+# Тест инициализации объектов
+# """
+# import pickle
+# import unittest
+# from pathlib import Path
+#
+#
+# class TestControllerInit(unittest.TestCase):
+#     """
+#     Тест инициализации объектов
+#     """
+#     def setUp(self) -> None:
+#         self._ctrl = ControllerInit
+#         self._ctrl.set_project_path(OUTPUT_PATH)
+#
+#     def test_1_download_project(self):
+#         """
+#         Тест загрузки объекта
+#         """
+#         object_ = {'foo': 'bar'}
+#         Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+#         file_path = Path.joinpath(OUTPUT_PATH, "tests.pkl")
+#         with open(file_path, 'wb') as output:
+#             pickle.dump(object_, output, pickle.HIGHEST_PROTOCOL)
+#         model = self._ctrl.download_model_if_exists("tests")
+#         assert model == object_, "Ошибка загрузки объекта"
+#
+#     def test_2_download_error(self):
+#         """
+#         Тест загрузки объекта
+#         """
+#         model = self._ctrl.download_model_if_exists("test2")
+#         assert not model, "Ошибка загрузки несуществующего объекта"
+#
+#     def tearDown(self):
+#         file_path = Path.joinpath(OUTPUT_PATH, "tests.pkl")
+#         if Path.exists(file_path):
+#             Path.unlink(file_path)
