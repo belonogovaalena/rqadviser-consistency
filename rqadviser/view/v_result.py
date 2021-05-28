c6f176983d4f035c3cf83f9b83bc953a9961d2c1
Белоногова Алена <belonogova@ians.aero>
2021-05-28 17:22:33 +0300

    Исправления в интерфейсе

--- rqadviser/view/v_result.py
+++ rqadviser/view/v_result.py
@@ -2,7 +2,8 @@
 Интерфейс таблицы с результатами анализа
 """
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QAbstractItemView, QAbstractScrollArea, QMainWindow, QTableView
+from PyQt5.QtWidgets import QAbstractItemView, QAbstractScrollArea, QGridLayout, QLabel, QMainWindow, QTableView, \
+    QWidget
 
 from rqadviser.model.m_table import TableModel
 
@@ -11,10 +12,11 @@ class ViewResult(QMainWindow):
     """
     Интерфейс таблицы с результатами анализа
     """
-    def __init__(self, df, parent=None):
+    def __init__(self, df, message="", parent=None):
         # pylint: disable=bad-super-call
         super(QMainWindow, self).__init__(parent)
         self._df = df
+        self._message = message
         self._create_table()
 
     def _create_table(self):
@@ -33,4 +35,9 @@ class ViewResult(QMainWindow):
         table.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
         table.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
         table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.setCentralWidget(table)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(QLabel(self._message), 0, 0)
+        grid_layout.addWidget(table, 1, 0)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        central_widget.setLayout(grid_layout)
